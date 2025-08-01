2906d3b1211a20ce39e03473f7f304b9cf71c579
JwowSquared <jdwagner1@hotmail.com>
2025-08-01 18:28:07 -0500

    changes for initial testing

--- JwowRandomizerSeed.lua
+++ JwowRandomizerSeed.lua
@@ -1,6 +1,6 @@
 local base = 0x02C24592
 local domain = "System Bus"
-local output_path = "jwow_randomizer_seed.txt"
+local output_path = "Randomizer Seed.txt"
 
 local previous_tid = -1
 local previous_sid = -1
@@ -15,19 +15,18 @@ while true do
     if i >= frames_to_wait then
         i = 0
 
-        local tid = memory.read_u16_le(base, domain)
-        local sid = memory.read_u16_le(base + 2, domain)
-        --local tid = memory.read_u32_le(base, domain)
+        --local tid = memory.read_u16_le(base, domain)
+        --local sid = memory.read_u16_le(base + 2, domain)
+        local tid = memory.read_u32_le(base, domain)
 
-        if tid ~= previous_tid or sid ~= previous_sid then
-        --if tid ~= previous_tid then
+        --if tid ~= previous_tid or sid ~= previous_sid then
+        if tid ~= previous_tid then
             previous_tid = tid
-            previous_sid = sid
+            --previous_sid = sid
 
             --local full_id = (sid * 0x10000) + tid
             local file = io.open(output_path, "w")
             file:write(string.format("%i\n", tid))
-            file:write(string.format("%i\n", sid))
             file:close()
 
             j = frames_to_wait
@@ -36,7 +35,7 @@ while true do
 
     if j > 0 then
         gui.drawRectangle(5, 5, 140, 20, "white", "black")
-        gui.drawText(10, 8, string.format("Jwow Feedback %i", j), "white", "black")
+        gui.drawText(10, 8, "I took your TID :)", "white", "black")
         j = j - 1
     end
 
